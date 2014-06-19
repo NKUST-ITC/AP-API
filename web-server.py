efd34b28c1cb9bac6f147e7676da34f69ecb824c
GrapherD <grapherd@gmail.com>
2014-06-19 12:31:48 +0800

    reform it

--- web-server.py
+++ web-server.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import function
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
+from flask import request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('finder.html')
+    return "Hello, World!"
+
+
 @app.route('/ap/login', methods=['POST'])
 def login_post():
     account = request.form['account']
@@ -16,16 +20,26 @@ def login_post():
     	return render_template('login.html')
     else:
     	return function.login(account,password)
-@app.route('/ap/curriculum', methods=['POST'])
-def AG_post():
-    account = request.form['account']
-    password = request.form['password']
-    AGID=request.form['AGID']
 
-    if account=="" and password=="" :
-    	return render_template('curriculum.html')
-    else:
-    	return function.curriculum(AGID,account,password)
+
+@app.route('/ap/query', methods=['GET', 'POST'])
+def query_post():
+    #password = request.form['password']
+    #ag_id = request.form['ag_id']
+
+    #print(account, password)
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        ag_id = request.form['ag_id']
+
+
+        
+
+
+    return render_template("query.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
