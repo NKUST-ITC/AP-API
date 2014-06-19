0b3255e23ce5d8fb12075d0664fbaa8b38cee33a
GrapherD <grapherd@gmail.com>
2014-06-19 12:54:50 +0800

    reform it

--- function.py
+++ function.py
@@ -1,38 +1,54 @@
 ﻿#-*- coding: utf-8 -*-
-import threading
+
 import requests
-import json
-import os
-import sys
-import uniout
-import collections
-from lxml import etree
-from bs4 import BeautifulSoup
 
-session = requests.session()
-logurl = "http://140.127.113.227/kuas/perchk.jsp"
-fnc_url="http://140.127.113.227/kuas/fnc.jsp"
-score_url="http://140.127.113.227/kuas/ag_pro/ag222.jsp?"
 
-def login(account, password):
-	payload = {"uid": account,"pwd": password}
-	response = session.post(logurl, data = payload)
+RANDOM_ID = "AG009"
 
-	if "密碼不正確" in response.content :
-		return "False"
-	else :
-		return "True"
-	
-def curriculum(AGID,account, password):
-	login(account,password)
-	ls_randnum=randnum("AG009")
-	raw_data={"arg01":"103","arg02":"1","arg03":account,"fncid":AGID,"uid":account,"ls_randnum":ls_randnum}
-	response = session.post(score_url, data = raw_data)
-	return response.content
-def randnum(fncid):
-	raw_data={"fncid":fncid,"sysyear":"103","syssms":"1","online":"okey","loginid":"1102108131"}
-	response = session.post(fnc_url, data = raw_data)
-	root = BeautifulSoup(response.text)
-	ans=root.findAll("input", {"type": "hidden", "name": "ls_randnum"})
-	ls_randnum=ans[0]['value']
-	return ls_randnum
+login_url = "http://140.127.113.227/kuas/perchk.jsp"
+fnc_url = "http://140.127.113.227/kuas/fnc.jsp"
+
+query_url = "http://140.127.113.227/kuas/%s_pro/%s.jsp?"
+
+s = requests.Session()
+def login( uid , pwd ):
+    response = s.post( "http://ap.kuas.edu.tw/kuas/perchk.jsp", data={"uid":uid,"pwd":pwd}).text
+
+    if response.find('f_index.html') != -1 :
+        print(str(uid) + "," + pwd)
+        with open("s.log", 'a') as l:
+            l.write( str(uid) + "," + pwd + "\n")
+        return True
+    else :
+        return False
+
+
+
+def query(qid, username, password, *args):
+    login(username, password)
+    ls_random = random_number(RANDOM_ID)
+
+    payload = {"arg01": "", "arg02": "", "arg03": "",
+                "fncid": "", "uid": "", "ls_randnum": ""}
+
+    payload['ls_randnum'] = ls_random
+    payload['fucid'] = qid
+
+    r = s.post(query_url % (qid[:2], qid), data=payload)
+
+    return r.content
+
+
+
+def random_number(fncid):
+    raw_data = {"fncid": fncid, "sysyear": "103", "syssms":
+                "1", "online": "okey", "loginid": "1102108131"}
+    response = session.post(fnc_url, data=raw_data)
+    root = BeautifulSoup(response.text)
+    ans = root.findAll("input", {"type": "hidden", "name": "ls_randnum"})
+    ls_randnum = ans[0]['value']
+    return ls_randnum
+
+
+if __name__ == "__main__":
+	login("1102108133", "111")
