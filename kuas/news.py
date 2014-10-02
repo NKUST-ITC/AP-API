d026602bce14bca96aca013a719b3566955cf24d
GrapherD <grapherd@gmail.com>
2014-10-02 15:24:45 +0800

    update news text and img

--- kuas/news.py
+++ kuas/news.py
@@ -24,8 +24,8 @@ def news():
             "睽違了一年 今年的大冒險再度啟航啦!!<br>"
             "你,是否有勇氣深入危機四伏的金字塔內?<br><br>"
             "◎康委大冒險2-圖坦卡門之影◎<br><br>"
-            "活動日：10/30、10/31<br>活動地點：高應大中正堂<br>活動時間：18:00~22:00</div>"
-            "<div><img style='display:block;margin-left:auto;margin-right:auto;margin-bottom:-15px;max-width:100%;min-height:180px;height:auto;' src='https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpa1/v/t34.0-12/s180x540/10634199_778926775498920_832242890_n.jpg?oh=71b544273f86732afdef098ff6929607&oe=542ECBC9&__gda__=1412411491_15f9688f6755a7c91a5e57233fcc8443'></img></div>"
+            "活動日：10/30、10/31<br>活動地點：高應大中正堂<br>活動時間：17:00~21:00</div>"
+            "<div><img style='display:block;margin-left:auto;margin-right:auto;margin-bottom:-15px;max-width:100%;min-height:150px;height:auto;' src='http://i.imgur.com/SkevurF.jpg'></img></div>"
         )
     news_url = "https://www.facebook.com/ReliveIsland"
 
