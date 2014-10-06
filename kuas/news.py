c4257f7ee8ad388cef90bc6f7763cbedf3dfa859
GrapherD <grapherd@gmail.com>
2014-10-06 15:47:45 +0800

    Update news to id2

--- kuas/news.py
+++ kuas/news.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-ENABLE = 0
-NEWS_ID = 0
+ENABLE = 1
+NEWS_ID = 2
 
 def news_status():
     return [ENABLE, NEWS_ID]
@@ -17,7 +17,8 @@ def news():
         news_tempalte: string
         news_url: string
     """
-
+   
+    """
     news_title = "活動消息"
     news_template = (
             "<div style='text-align:center;margin-top:-15px'>"
@@ -28,6 +29,16 @@ def news():
             "<div><img style='display:block;margin-left:auto;margin-right:auto;margin-bottom:-15px;max-width:100%;min-height:150px;height:auto;' src='http://i.imgur.com/SkevurF.jpg'></img></div>"
         )
     news_url = "https://www.facebook.com/ReliveIsland"
+    """
 
+    news_title = "停電公告"
+    news_template = (
+            "<div style='text-align:center;margin-top:-15px;margin:0 auto;width:85%;'>"
+            "因10月10日(五)計網中心全機房斷電,新增不斷電系統與數位電表。<br><br>"
+            "故10月10日當天校車預約系統無法預約交通車。<br><br>"
+            "請有要預約10月10日交通車的師生，在<font color='red'>10月9日中午12點前</font>上校車預約系統預約，以避免沒有校車可搭乘."
+            "</div>"
+            )
+    news_url = ""
 
     return [ENABLE, NEWS_ID, news_title, news_template, news_url]
