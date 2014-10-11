6b6a3772f9295917a01b59a63e2b5cb6fb4710ca
GrapherD <grapherd@gmail.com>
2014-10-11 21:23:27 +0800

    Add bus query to ap_test.py

--- ap_test.py
+++ ap_test.py
@@ -45,10 +45,19 @@ def process():
 
     
     try:
+        r = s.post(URL + "bus/query", data={"date": "2014-10-30"})
+        print("[*] Bus Query success")
+        print("    - %s" % r.text[:100])
+    except Exception as e:
+        print("[-] Bus Query fatal error")
+        print(e)
+        return 
+
+    try:
         r = s.post(URL + "leave", data={"arg01": "102", "arg02": "01"})
         if not r.text.startswith("false"):
             print("[*] Leave Query success")
-            print(r.text)
+            print("    - %s" % r.text)
         else:
             print("[-] Leave Query fail")
             return
