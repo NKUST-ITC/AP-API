56094a22dbb7fa1221327175f56b46d5c39748f2
GrapherD <grapherd@gmail.com>
2014-11-15 01:13:25 +0800

    Update news

--- kuas/news.py
+++ kuas/news.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ENABLE = 1
-NEWS_ID = 5
+NEWS_ID = 7
 
 def news_status():
     return [ENABLE, NEWS_ID]
@@ -18,17 +18,17 @@ def news():
         news_url: string
     """
    
-    news_title = "活動消息"
+    news_title = "三合一選舉"
     news_template = (
             "<div style='text-align:center;margin-top:-15px'>"
-            "◎康委大冒險2-圖坦卡門之影◎<br><br>"
-            "即將於本週禮拜四、五登場！<br>"
-            "日期為：10/30、31日 晚上5～9點<br>"
-            "可以當場換票 10張發票換一張門票<br><br>"
-            "你準備好面對法老的冒險了嗎！"
-            "<div><img style='display:block;margin-left:auto;margin-right:auto;margin-bottom:-15px;max-width:100%;min-height:150px;height:auto;' src='http://i.imgur.com/SkevurF.jpg'></img></div>"
+            "◎104級三合一選舉學生會正副會長候選人政見發表會 - 逐字稿◎<br><br>"
+            "1號候選人 — 呂紹榕 x 詹濬鍵<br>"
+            "2號候選人 — 江敬全 x 邱博雅<br><br>"
+            "錯過了正副會長政見發表會<br>想要了解兩組候選人說了些什麼嗎？<br><br>"
+            "點選立即前往，透過逐字稿<br>了解兩組候選人的政見吧！"
+            "<div><img style='display:block;margin-left:auto;margin-right:auto;margin-bottom:-15px;max-width:100%;min-height:150px;height:auto;' src='http://i.imgur.com/YsV56MB.png'></img></div>"
         )
-    news_url = "https://www.facebook.com/ReliveIsland"
+    news_url = "http://goo.gl/5ozyWe"
 
 
     return [ENABLE, NEWS_ID, news_title, news_template, news_url]
