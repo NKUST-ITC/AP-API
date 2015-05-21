b23edff3803f69893d3f4926cfea52e166850479
GrapherD <grapherd@gmail.com>
2015-05-21 22:50:37 +0800

    Update news

--- kuas/news.py
+++ kuas/news.py
@@ -19,36 +19,13 @@ def random_by_weight(p):
 
 def random_news():
     news_list = [
-         {
-            "news_title": "Dcard coming soon",
-            "news_image": "http://i.imgur.com/wDNlEaU.jpg",
-            "news_url": "https://www.facebook.com/events/186383611572196/",
-            "news_content": "",
-            "news_weight": 0
-        },
-        {
-            "news_title": "2015高應大校園歌唱大賽",
-            "news_image": "http://i.imgur.com/oRRaM6H.jpg",
-            "news_url": "https://www.facebook.com/pages/2015%E9%AB%98%E6%87%89%E5%A4%A7%E6%A0%A1%E5%9C%92%E6%AD%8C%E5%94%B1%E5%A4%A7%E8%B3%BD/503404693131297",
-            "news_content": "",
-            "news_weight": 0
-
-        },
         {
             "news_title": "航海王奪寶 模咒逃走中",
             "news_image": "http://i.imgur.com/LVcpWi9.jpg",
             "news_url": "http://goo.gl/forms/6BbIyZXHdd",
             "news_content": "",
             "news_weight": 0
-        },
-        {
-            "news_title": "高應大資訊研習社",
-            "news_image": "http://i.imgur.com/cSHCYfZ.png",
-            "news_url": "https://www.facebook.com/KUASITC/photos/a.750277898402920.1073741828.735951703168873/759733750790668/?type=1",
-            "news_content": "",
-            "news_weight": 0
         }
-
     ]
 
     if NEWS_DEBUG:
