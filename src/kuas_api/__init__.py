0fb40d58bd5a0f5ff9c3e443f33bf1850439bc07
GrapherD <grapherd@gmail.com>
2015-08-29 17:55:38 +0800

    delete unuse import

--- src/kuas_api/__init__.py
+++ src/kuas_api/__init__.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
-
-import os
-import json
-from functools import wraps
-
-import requests
-import kuas_api.kuas.ap as ap
-import kuas_api.kuas.user as user
-import kuas_api.kuas.function as function
-
-from flask import Flask, render_template, request, session
-from flask_cors import *
+from flask import Flask
 
 __version__ = "2.0"
 
-android_version = "1.5.4"
-android_donate_version = "2.0.0"
-ios_version = "1.4.3"
 
 app = Flask(__name__)
 app.config.from_object("config")
