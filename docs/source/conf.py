ea7425908f873db41f28e6c9c4fcb3626f7bd328
GrapherD <grapherd@gmail.com>
2015-09-04 14:18:36 +0800

    Remove numpydoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'numpydoc'
+#    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
