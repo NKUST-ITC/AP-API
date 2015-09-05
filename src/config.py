86fb3a8b1965862101449898a2d86a084d0accec
GrapherD <grapherd@gmail.com>
2015-09-05 18:50:27 +0800

    Update flask config

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-from datetime import timedelta
+#from datetime import timedelta
 
 
-DEBUG = True
+#DEBUG = True
 SECRET_KEY = os.urandom(24)
-SESSION_COOKIE_HTTPONLY = False
-PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
-
-CORS_ORIGINS = "http://localhost:8000"
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+#PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
+#CORS_ORIGINS = "http://localhost:8000"
