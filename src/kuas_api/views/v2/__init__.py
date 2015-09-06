6d619d0f7ede943a116bf0053d41bac868e211b3
GrapherD <grapherd@gmail.com>
2015-09-07 00:35:47 +0800

    Port notification from v1 to v2, change endpoints

--- src/kuas_api/views/v2/__init__.py
+++ src/kuas_api/views/v2/__init__.py
@@ -21,13 +21,15 @@ from kuas_api.views.v2.utils import routes as utils_routes
 from kuas_api.views.v2.ap import routes as ap_routes
 from kuas_api.views.v2.bus import routes as bus_routes
 from kuas_api.views.v2.leave import routes as leave_routes
+from kuas_api.views.v2.notifications import routes as notifications_routes
 
 # Dirty from #593
 routes = (
     utils_routes +
     ap_routes +
     bus_routes +
-    leave_routes
+    leave_routes +
+    notifications_routes
 )
 
 for r in routes:
