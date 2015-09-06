dcba7ba611f2d2a5784ad5442db76cafc0462c9b
GrapherD <grapherd@gmail.com>
2015-09-07 00:34:49 +0800

    Change notificationi return json struct

--- src/kuas_api/kuas/notification.py
+++ src/kuas_api/kuas/notification.py
@@ -6,18 +6,21 @@ import requests
 
 NOTIFICATION_URL = "http://www.kuas.edu.tw/files/501-1000-1003-%d.php"
 
+
 def get(page=1):
-    r = requests.get(NOTIFICATION_URL %(page))
+    r = requests.get(NOTIFICATION_URL % (page))
     r.encoding = "utf-8"
 
     root = etree.HTML(r.text)
     trs = root.xpath("//tr[starts-with(@class, 'row')]")
-    return trs
+
     result = []
     for tr in trs:
         a = tr.xpath("td//a")[0].values()[1]
-        tr = list(filter(lambda x: x, map(lambda x: x.replace("\t", "").replace("\n", ""), tr.itertext())))
-        tr = tr[1:]
+        tr = list(filter(lambda x: x, map(
+            lambda x: x.replace("\t", "").replace("\n", ""), tr.itertext())))
+
+        tr = dict(zip(("id", "date", "title", "department"), tr))
 
         result.append({'link': a, 'info': tr})
 
