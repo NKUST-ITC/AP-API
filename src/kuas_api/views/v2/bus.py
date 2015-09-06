397633f43ef2ed8110003b1233a269dc7c3ecdfc
GrapherD <grapherd@gmail.com>
2015-09-07 00:36:25 +0800

    Delete error docstring

--- src/kuas_api/views/v2/bus.py
+++ src/kuas_api/views/v2/bus.py
@@ -44,12 +44,6 @@ def timetables():
     :query string from: The start station you want to query. (not impl yet)
     :statuscode 200: no error
 
-    .. csv-table::
-       "Albatross", 2.99, "On a stick!"
-       "Crunchy Frog", 1.49, "If we took the bones out, it wouldn't be
-       crunchy, now would it?"
-       "Gannet Ripple", 1.99, "On a stick!"
-
 
     **Requires authentication?**
 
