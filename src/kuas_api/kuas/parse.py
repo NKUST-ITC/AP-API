3adf23beb8ca4033de67a8e5d4c0c396a8628f01
GrapherD <grapherd@gmail.com>
2015-09-09 00:28:31 +0800

    Change ap parse

--- src/kuas_api/kuas/parse.py
+++ src/kuas_api/kuas/parse.py
@@ -3,6 +3,10 @@
 from lxml import etree
 
 
+sections_time = []
+weekdays_abbr = []
+
+
 def parse(fncid, content):
     if fncid in parse_function:
         return parse_function[fncid](content)
@@ -28,82 +32,56 @@ def course(cont):
         center = ""
         center_text = ""
 
-
     # Return if no course data
     if center_text.startswith(u'學生目前無選課資料!'):
         return [[], False, False, center_text]
 
-
     tbody = root.xpath("//table")[-1]
 
-    course_table = {}
-    for r_index, r in enumerate(tbody[1:]):
-        row = {}
-        for index, c in enumerate(r.xpath("td")):
-            r = list(filter(lambda x: x != u"\xa0", c.itertext()))
-            
-            if index == 0:
-                row['time'] = r[0].replace(" ", "")
-            else:
-                row[index] = {
-                        "course_name": "",
-                        "course_teacher": "",
-                        "course_classroom": ""
-                    }
+    course_table = []
+    for sections, r in enumerate(tbody[1:]):
+        section = ""
+        start_time = ""
+        end_time = ""
 
-                if r:
-                    while len(r) < 3:
-                        r.append("")
+        for weekends, c in enumerate(r.xpath("td")):
+            classes = {"title": "", "date": {}, "location": {}, "instructors": []}
 
-                    row[index]["course_name_simple"] = r[0][:2]
-                    row[index]["course_name"] = r[0]
-                    row[index]["course_teacher"] = r[1]
-                    row[index]["course_classroom"] = r[2]
+            r = list(
+                filter(
+                    lambda x: x,
+                    map(lambda x: x.replace("\xa0", ""), c.itertext())
+                )
+            )
 
-        course_table[r_index] = row
+            if not weekends:
+                section = r[0]
+                start_time = ""
+                end_time = ""
 
+                if len(r) > 1:
+                    start_time, end_time = r[1].split("-")
 
-    # Check if over 8th didn't have class
-    token_b = False
-    token_night = False
+                continue
 
+            if not r:
+                continue
 
-    for r in course_table:
-        for c in course_table[r]:
-            if r > 9 and c != "time" and course_table[r][c]['course_name']:
-                if r == 10:
-                    token_b = True
-                else:
-                    #print(course_table[r][c]['course_name'])
-                    token_night = True
+            classes["title"] = r[0]
+            classes["date"]["start_time"] = start_time
+            classes["date"]["end_time"] = end_time
+            classes["date"]["weekday"] = " MTWRFSH"[weekends]
+            classes["date"]["section"] = section
 
-    #print(token_b, token_night)
+            classes["location"]["building"] = ""
+            classes["location"]["room"] = r[2] if len(r) > 2 else ""
 
+            classes["instructors"].append(r[1])
 
-    if token_night: 
-        pass
-    elif token_b:
-        for i in [11, 12, 13, 14]:
-            del course_table[i]
-    else:
-        for i in [10, 11, 12, 13, 14]:
-            del course_table[i]
+            course_table.append(classes)
 
-        
+    return course_table
 
-    # Check Saturday and Sunday class
-    have_saturday = False
-    have_sunday = False
-    
-    for r in course_table:
-        if not isinstance(course_table[r], bool) and course_table[r][6]["course_name"]:
-            have_saturday = True
-        if not isinstance(course_table[r], bool) and course_table[r][7]["course_name"]:
-            have_sunday = True
-
-
-    return [course_table, have_saturday, have_sunday, False]
-      
 
 def score(cont):
     root = etree.HTML(cont)
@@ -117,7 +95,6 @@ def score(cont):
         tbody = ""
         center = ""
         center_text = ""
-        
 
     if center_text.startswith(u'目前無學生個人成績資料'):
         return [[], [], center_text]
@@ -136,7 +113,7 @@ def score(cont):
         row["middle_score"] = r[6]
         row["final_score"] = r[7]
         row["remark"] = r[8]
-        
+
         score_table.append(row)
 
     total_score = root.xpath("//div")[-1].text.replace(u"　　　　", " ").split(" ")
@@ -148,5 +125,5 @@ parse_function = {"ag222": course, "ag008": score}
 
 
 if __name__ == "__main__":
-    #print(course(open("c.html").read()))
+    # print(course(open("c.html").read()))
     pass
