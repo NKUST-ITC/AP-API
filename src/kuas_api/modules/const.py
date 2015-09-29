138df31dc628daad0c60f062b05774d6c7d4338d
GrapherD <grapherd@gmail.com>
2015-09-29 09:40:44 +0800

    Change android version to 2.1.3

--- src/kuas_api/modules/const.py
+++ src/kuas_api/modules/const.py
@@ -1,14 +1,14 @@
 #-*- coding: utf-8 -*-
-
 device_version = {
-    "android": "2.1.2",
+    "android": "2.1.3",
     "android_donate": "2.1.2",
-    "ios": "1.4.3"
+    "ios": "1.6.0"
 }
 
 
 # Token duration in seconds
 token_duration = 3600
+serect_key = "usapoijupojfa;dsj;lv;ldakjads;lfkjapoiuewqprjf"
 
 # HTTP Status Code
 ok = 200
