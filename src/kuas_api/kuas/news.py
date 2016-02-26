bf6edfa0ef3ecbc47e4db75f81f7d74705e44905
GrapherD <grapherd@gmail.com>
2016-02-26 13:15:00 +0800

    update news

--- src/kuas_api/kuas/news.py
+++ src/kuas_api/kuas/news.py
@@ -20,26 +20,12 @@ def random_by_weight(p):
 def random_news():
     news_list = [
         {
-            "news_title": "第八屆搖音新生盃",
-            "news_image": "http://i.imgur.com/ziFEckz.jpg",
-            "news_url": "https://www.facebook.com/KUAS.RockMusicCompany/photos/pb.240024276068437.-2207520000.1446562914./945089888895202/?type=3&theater",
+            "news_title": "第八屆泰北團-夢想，「泰」不一樣",
+            "news_image": "http://i.imgur.com/iNbbd4B.jpg",
+            "news_url": "https://docs.google.com/forms/d/11Awcel_MfPeiEkl7zQ0MldvnAw59gXKLecbIODPOaMs/viewform?edit_requested=true",
             "news_content": "",
             "news_weight": 4
-        },
-        {
-            "news_title": "期末成果發表會 [春韻]",
-            "news_image": "http://i.imgur.com/To8vitb.jpg",
-            "news_url": "https://www.facebook.com/events/806882852767987/",
-            "news_content": "",
-            "news_weight": 4
-        },
-        {
-            "news_title": "我的藝響實驗舞臺-藝起來藝文活動",
-            "news_image": "http://i.imgur.com/eLbq9KU.jpg",
-            "news_url": "https://www.facebook.com/kuasfunart/?pnref=story&__mref=message_bubble",
-            "news_content": "",
-            "news_weight": 4
-        },
+        }
     ]
 
     if NEWS_DEBUG:
