81946edeb832eefbebf739d57057d0698ff43f11
RainVisitor <abc873693@gmail.com>
2019-05-16 13:05:38 +0800

    API v3 document (#47)
    * Fix secret-key (#40)
    
    Thanks
    
    * Fix docker timezone error (#41)
    
    * Hotfix/docker timezone (#42)
    
    * Fix docker timezone error
    
    * Fix docker-compose.yml
    
    * Initial apidoc v3
    
    * Create a api page
    
    * Add some api doc
    
    Update:
     some text
    Add:
    user/midterm-alerts 
    user/reward-and-punishments doc
    leaves/submit/info
    
    * Update api-page.html
    
    * Add github corner
    
    * Add graduation-threshold and violation-records api doc
    
    Update:
     bus reserve api doc
    
    * Fix auth spec
    
    * Combined user/info & user/picture
    
    * Fix midterm-alerts need parameters (year,semester)
    
    * Add leaves/submit/info teacherList
    
    * Add library info
    
    * Add leave/submit
    
    * Fix leaves/submit/info teacher type.
    
    * Add null-class
    
    * Fix library info example
    
    * Fix some spec
    
    * Fix User schema
    
    * Add leave schema & library schema
    
    * Add News schema
    
    * Fix some example & schema
    
    * Fix semesters example

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 DEBUG = True
-SECRET_KEY = "sdfsdhpmrgewf"
+SECRET_KEY = "change secret key in production"
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
 #PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
