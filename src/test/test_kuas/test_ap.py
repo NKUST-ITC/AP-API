1e112db816456b16d49880ee9fe516192da7efee
takidog <buluni.ha@gmail.com>
2019-05-20 17:35:26 +0800

    Remove flask framework

--- src/test/test_kuas/test_ap.py
+++
@@ -1,8 +0,0 @@
-import config
-import unittest
-
-
-class FooTest(unittest.TestCase):
-    def test_foo(self):
-        self.assertTrue(1 == 1)
-        self.assertTrue(config.UNITTEST_USERNAME)
--- src/web-server.py
+++
@@ -1,6 +0,0 @@
-from kuas_api import app
-from flask_compress import Compress
-
-if __name__ == "__main__":
-    Compress(app)
-    app.run(host="0.0.0.0", port=5001, debug=True, ssl_context='adhoc')
